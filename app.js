8518f6c66ebc840d907496845df23c73a5f9ad42
Igor <ig.zab.ru@gmail.com>
2021-07-07 13:46:53 +0200

    update

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const slides = document.querySelectorAll('.slide')
+const slides = document.querySelectorAll(".slide");
 
 function clearActiveClass() {
-	slides.forEach( (slide) => {
-		slide.classList.remove('active')
-	})
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
 }
 
 for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClass()
-		slide.classList.add('active')
-	})
+  slide.addEventListener("click", () => {
+    if (slide.classList.contains("active")) {
+      clearActiveClass();
+    } else {
+      clearActiveClass();
+      slide.classList.add("active");
+    }
+  });
 }
